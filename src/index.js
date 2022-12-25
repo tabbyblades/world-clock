e5e26b492d6b1b041da649b1d3f3aa13121ed995
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2022-12-25 16:29:25 +0900

    Added moment.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let edinburghElement = document.querySelector("#edinburgh");
 let edinburghDateElement = edinburghElement.querySelector(".date");
 let edinburghTimeElement = edinburghElement.querySelector(".time");
-edinburghDateElement.innerHTML= "the date will go here";
+edinburghDateElement.innerHTML= moment().format("MMMM Do YYYY");
 edinburghTimeElement.innerHTML = "the time will go here";
