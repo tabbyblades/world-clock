2bfa73e64297b428d518cfadad3d38b8ec4fb23e
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2022-12-26 08:52:59 +0900

    created newyorkElement

--- src/index.js
+++ src/index.js
@@ -18,3 +18,10 @@ let parisTimeElement = parisElement.querySelector(".time");
 let parisTime = moment().tz("Europe/Paris");
 parisDateElement.innerHTML= moment().format("MMMM Do YYYY");
 parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+
+let newyorkElement = document.querySelector("#newyork");
+let newyorkDateElement = newyorkElement.querySelector(".date");
+let newyorkTimeElement = newyorkElement.querySelector(".time");
+let newyorkTime = moment().tz("US/Eastern");
+newyorkDateElement.innerHTML= moment().format("MMMM Do YYYY");
+newyorkTimeElement.innerHTML = newyorkTime.format("h:mm:ss [<small>]A[</small>]");
