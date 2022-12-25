e7a16bde57c7050de0722ff28a065bfc50676bc1
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2022-12-26 08:45:35 +0900

    created seoulElement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 let edinburghElement = document.querySelector("#edinburgh");
 let edinburghDateElement = edinburghElement.querySelector(".date");
 let edinburghTimeElement = edinburghElement.querySelector(".time");
+let edinburghTime = moment().tz("GB");
 edinburghDateElement.innerHTML= moment().format("MMMM Do YYYY");
-edinburghTimeElement.innerHTML = "the time will go here";
+edinburghTimeElement.innerHTML = edinburghTime.format("h:mm:ss [<small>]A[</small>]");
+
+let seoulElement = document.querySelector("#seoul");
+let seoulDateElement = seoulElement.querySelector(".date");
+let seoulTimeElement = seoulElement.querySelector(".time");
+let seoulTime = moment().tz("Asia/Seoul");
+seoulDateElement.innerHTML= moment().format("MMMM Do YYYY");
+seoulTimeElement.innerHTML = seoulTime.format("h:mm:ss [<small>]A[</small>]");
