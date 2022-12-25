b8f47ff43551645e110db6b360dc7b0563119f66
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2022-12-25 16:21:40 +0900

    Created edinburghElement

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let edinburghElement = document.querySelector("#edinburgh");
+let edinburghDateElement = edinburghElement.querySelector(".date");
+let edinburghTimeElement = edinburghElement.querySelector(".time");
+edinburghDateElement.innerHTML= "the date will go here";
+edinburghTimeElement.innerHTML = "the time will go here";
