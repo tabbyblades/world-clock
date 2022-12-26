55528a225b754d4f70f0b5644fdbb953af603e22
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2022-12-26 13:03:39 +0900

    changed edinburghElement to londonElement and created updateCity function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 function updateTime(){
 
-let edinburghElement = document.querySelector("#edinburgh");
-let edinburghDateElement = edinburghElement.querySelector(".date");
-let edinburghTimeElement = edinburghElement.querySelector(".time");
-let edinburghTime = moment().tz("GB");
-edinburghDateElement.innerHTML= moment().format("MMMM Do YYYY");
-edinburghTimeElement.innerHTML = edinburghTime.format("h:mm:ss [<small>]A[</small>]");
+let londonElement = document.querySelector("#london");
+let londonDateElement = londonElement.querySelector(".date");
+let londonTimeElement = londonElement.querySelector(".time");
+let londonTime = moment().tz("Europe/London");
+londonDateElement.innerHTML= moment().format("MMMM Do YYYY");
+londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
 
 let seoulElement = document.querySelector("#seoul");
 let seoulDateElement = seoulElement.querySelector(".date");
@@ -31,3 +31,21 @@ newyorkTimeElement.innerHTML = newyorkTime.format("h:mm:ss [<small>]A[</small>]"
 
 updateTime()
 setInterval(updateTime, 1000);
+
+function updateCity(event){
+    let cityTimezone = event.target.value;
+    let cityTime = moment().tz(cityTimezone);
+    let cityName = cityTimezone.replace("_", " ").split("/")[1];
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML= 
+    `<div class="row m-3 city">
+    <div class="col-md-6"><h2>${cityName}</h2>
+    <h3 class="date">${cityTime.format("MMMM Do YYYY")}</h3></div>
+    <div class="col-md-6 time"><h3>${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small>
+</h3></div>
+</div>`;
+}
+
+let citySelectElement = document.querySelector("#city");
+
+citySelectElement.addEventListener("change", updateCity);
